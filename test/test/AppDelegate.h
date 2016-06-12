fc09382fd252bb4a6355a11bb841ee96ee193792
癸酉欧阳 <ouyanguiyou@jiangnanbuyidemacbook-pro.local>
2016-06-12 15:14:43 +0800

    init

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 江南布衣 on 16/6/12.
+//  Copyright © 2016年 江南布衣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 江南布衣 on 16/6/12.
+//  Copyright © 2016年 江南布衣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
